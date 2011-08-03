7ee4ba25968cf9442cb6390d18bb5ad676d6327e
ChernobylStalker <William@chernobyl360@live.com>
2011-08-03 10:22:18 -0400

    implemented playerlistener with interact event to get smoke.

--- src/us/william/blacklung/BLBlockListener.java
+++ src/us/william/blacklung/BLBlockListener.java
@@ -1,7 +1,6 @@
 package us.william.blacklung;
 
 import org.bukkit.ChatColor;
-import org.bukkit.Effect;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.block.BlockBreakEvent;
@@ -9,19 +8,18 @@ import org.bukkit.event.block.BlockListener;
 
 public class BLBlockListener extends BlockListener
 {
-  public static BlackLung plugin;
+	public static BlackLung plugin;
 
-  public BLBlockListener(BlackLung instance)
-  {
-    plugin = instance;
-  }
-  public void onBlockBreak(BlockBreakEvent event) {
-    Player player = event.getPlayer();
-    if (((player.getItemInHand().getType() == Material.DIAMOND_PICKAXE) || (event.getPlayer().getItemInHand().getType() != Material.GOLD_PICKAXE) || (event.getPlayer().getItemInHand().getType() != Material.IRON_PICKAXE) || (event.getPlayer().getItemInHand().getType() != Material.STONE_PICKAXE) || (event.getPlayer().getItemInHand().getType() != Material.WOOD_PICKAXE)) && 
-      (event.getBlock().getType() == Material.COAL_ORE)) {
-    	player.getWorld().playEffect(player.getLocation(), Effect.SMOKE, 0);
-      player.damage(BlackLung.COAL_DAMAGE);
-      player.sendMessage(ChatColor.GRAY + BlackLung.DAMAGE_MESSAGE);
-    }
-  }
+	public BLBlockListener(BlackLung instance)
+	{
+		plugin = instance;
+	}
+	public void onBlockBreak(BlockBreakEvent event) {
+		Player player = event.getPlayer();
+		if (((player.getItemInHand().getType() == Material.DIAMOND_PICKAXE) || (event.getPlayer().getItemInHand().getType() != Material.GOLD_PICKAXE) || (event.getPlayer().getItemInHand().getType() != Material.IRON_PICKAXE) || (event.getPlayer().getItemInHand().getType() != Material.STONE_PICKAXE) || (event.getPlayer().getItemInHand().getType() != Material.WOOD_PICKAXE)) && 
+				(event.getBlock().getType() == Material.COAL_ORE)) {
+			player.damage(BlackLung.COAL_DAMAGE);
+			player.sendMessage(ChatColor.GRAY + BlackLung.DAMAGE_MESSAGE);
+		}
+	}
 }
