d954ff274493d9bd52b4d4649c53276d8d5d1fdd
ChernobylStalker <William@chernobyl360@live.com>
2011-08-07 22:27:33 -0400

    updated alot! fires/explosions

--- src/us/william/blacklung/BLBlockListener.java
+++ src/us/william/blacklung/BLBlockListener.java
@@ -1,25 +1,73 @@
 package us.william.blacklung;
 
+import java.util.Random;
+
 import org.bukkit.ChatColor;
+import org.bukkit.Effect;
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockDamageEvent;
 import org.bukkit.event.block.BlockListener;
 
 public class BLBlockListener extends BlockListener
 {
-	public static BlackLung plugin;
-
-	public BLBlockListener(BlackLung instance)
-	{
-		plugin = instance;
-	}
-	public void onBlockBreak(BlockBreakEvent event) {
-		Player player = event.getPlayer();
-		if (((player.getItemInHand().getType() == Material.DIAMOND_PICKAXE) || (event.getPlayer().getItemInHand().getType() != Material.GOLD_PICKAXE) || (event.getPlayer().getItemInHand().getType() != Material.IRON_PICKAXE) || (event.getPlayer().getItemInHand().getType() != Material.STONE_PICKAXE) || (event.getPlayer().getItemInHand().getType() != Material.WOOD_PICKAXE)) && 
-				(event.getBlock().getType() == Material.COAL_ORE)) {
-			player.damage(BlackLung.COAL_DAMAGE);
-			player.sendMessage(ChatColor.GRAY + BlackLung.DAMAGE_MESSAGE);
-		}
-	}
+    public static BlackLung plugin;
+    int time;
+    
+    Random r = new Random();
+    public BLBlockListener(BlackLung instance)
+    {
+        plugin = instance;
+    }
+    public void onBlockBreak(BlockBreakEvent event){
+        if (event.getBlock().getType() == (Material.COAL_ORE)) {
+            if(true)
+            {
+            time = r.nextInt(BlackLung.RANDOM_BOOM);
+            }
+                Player p = event.getPlayer();
+                World w = p.getWorld();
+                Block b = event.getBlock();
+                w.createExplosion(b.getLocation(), BlackLung.BOOM_RADIUS);
+                p.setFireTicks(BlackLung.FIRE_TICKER);
+            }
+        }
+    public void onBlockDamage(BlockDamageEvent event){
+        Player player = event.getPlayer();
+        if (((player.getItemInHand().getType() == Material.DIAMOND_PICKAXE) || (event.getPlayer().getItemInHand().getType() != Material.GOLD_PICKAXE) || (event.getPlayer().getItemInHand().getType() != Material.IRON_PICKAXE) || (event.getPlayer().getItemInHand().getType() != Material.STONE_PICKAXE) || (event.getPlayer().getItemInHand().getType() != Material.WOOD_PICKAXE)) &&
+                (event.getBlock().getType() == Material.COAL_ORE)) {
+            Location loc = player.getLocation();
+            World w = loc.getWorld();
+            int repeat = 0;
+            while(repeat < 1){
+                for(double x = (loc.getX() - 3); x <= (loc.getX() + 3); x++){
+                    for(double y = (loc.getY() - 3); y <= (loc.getY() + 3); y++){
+                        for(double z = (loc.getZ() - 3); z <= (loc.getZ() + 3); z++){
+                            w.playEffect(new Location(w, x, y, z), Effect.SMOKE, 1);
+                        }
+                    }
+                }
+                repeat++;
+            }
+            if (player.getInventory().getHelmet().getTypeId() != 298)
+                if (player.getInventory().getHelmet().getTypeId() != 302)
+                    if (player.getInventory().getHelmet().getTypeId() != 306)
+                        if (player.getInventory().getHelmet().getTypeId() != 314)
+                            if (player.getInventory().getHelmet().getTypeId() != 310)
+                                player.sendMessage(ChatColor.GRAY + BlackLung.DAMAGE_MESSAGE);
+            if (player.getInventory().getHelmet().getTypeId() != 298)
+                if (player.getInventory().getHelmet().getTypeId() != 302)
+                    if (player.getInventory().getHelmet().getTypeId() != 306)
+                        if (player.getInventory().getHelmet().getTypeId() != 314)
+                            if (player.getInventory().getHelmet().getTypeId() != 310)
+                                player.damage(BlackLung.COAL_DAMAGE);
+        }
+        if (player.getItemInHand().getType() == Material.AIR){
+            event.isCancelled();
+        }
+    }
 }
--- src/us/william/blacklung/BlackLung.java
+++ src/us/william/blacklung/BlackLung.java
@@ -10,8 +10,11 @@ public class BlackLung extends JavaPlugin
 {
 	private final BLBlockListener BlockListener = new BLBlockListener(this);
 	public Configuration config;
-	protected static int COAL_DAMAGE = 1; //where 1 is the default value
-	protected static String DAMAGE_MESSAGE = "*cough*";//again, default value	 
+	protected static int COAL_DAMAGE = 1; //Damage Player Will Receive when mining COAL!
+	protected static int RANDOM_BOOM = 1; //higher the # Less the chance of BOOM!
+	protected static int FIRE_TICKER = 200; //Seconds Player Will Burn
+	protected static int BOOM_RADIUS = 2; //Radius Of BOOM!
+	protected static String DAMAGE_MESSAGE = "*cough*";//Message They Will Receive!	 
 	public static Logger log;
 
 	public void onEnable()
@@ -21,6 +24,7 @@ public class BlackLung extends JavaPlugin
 		log.info(getDescription().getName() + " version " + getDescription().getVersion() + " loaded.");
 
 		PluginManager pm = getServer().getPluginManager();
+		pm.registerEvent(Event.Type.BLOCK_DAMAGE, this.BlockListener, Event.Priority.Normal, this);
 		pm.registerEvent(Event.Type.BLOCK_BREAK, this.BlockListener, Event.Priority.Normal, this);
 	}
 	public void onDisable() {
@@ -30,7 +34,10 @@ public class BlackLung extends JavaPlugin
 		Configuration config = getConfiguration();
 		config.load();//This will load the config file, and if it doesn't exist, create a new one without any hassle
 		//Now, to load the default values (and set the new values if they've been changed)
-		COAL_DAMAGE = config.getInt("BlackLung.Coal-Damage", COAL_DAMAGE); //the first parameter is just what You want to display as the node in the configuration file
+		COAL_DAMAGE = config.getInt("BlackLung.Coal-Damage", COAL_DAMAGE); 
+		FIRE_TICKER = config.getInt("BlackLung.Fire-Ticker", FIRE_TICKER); 
+		RANDOM_BOOM = config.getInt("BlackLung.Random-Boom", RANDOM_BOOM);
+		BOOM_RADIUS = config.getInt("BlackLung.Boom-Radius", BOOM_RADIUS);
 		DAMAGE_MESSAGE = config.getString("BlackLung.Damage-Message", DAMAGE_MESSAGE);
 		config.save(); //save the new configuration file
 	}
